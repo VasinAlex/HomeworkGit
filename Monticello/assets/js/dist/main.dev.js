d2ab3fbe749e582627b3284e52408dfc1ce4b9d9
VasinAlex <12volth@gmail.com>
2023-03-16 14:49:08 +0200

    add response for slider monticello

--- Monticello/assets/js/dist/main.dev.js
+++ Monticello/assets/js/dist/main.dev.js
@@ -157,12 +157,14 @@ function getNews() {
       }, {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       }, {
         breakpoint: 767,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }]
     });
--- Monticello/assets/js/main.js
+++ Monticello/assets/js/main.js
@@ -193,13 +193,14 @@ const API_KEY = "5684706ccfcecf89d161cf52ec2a382d";//gnews
 												breakpoint: 1024,
 												settings: {
 													slidesToShow: 2,
+													slidesToScroll: 2,
 												}
 											},
 											{
 												breakpoint: 767,
 												settings: {
 													slidesToShow: 1,
-						
+													slidesToScroll: 1,
 												}
 											}
 										]
